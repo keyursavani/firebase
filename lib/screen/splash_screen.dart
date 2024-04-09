7f7e5e51728187662717f86f8fe286831bab71fb
keyur savani <savanikeyur1011@gmail.com>
2024-04-09 21:33:16 +0530

    final commit

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget{
   const SplashScreen({super.key});
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
